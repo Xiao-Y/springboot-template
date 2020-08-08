cad3c645bd2bb7d5a8e88d2578ec60aab8e25000
liuyongtao <lyongtao123@126.com>
2020-08-08 20:18:28 +0800

    提交目录结构

---
+++ src/main/java/com/billow/template/SpriingbootTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.billow.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpriingbootTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpriingbootTemplateApplication.class, args);
+	}
+
+}
